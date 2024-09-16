c7194c56bede81bec84af79bb41d8ebb3b22d5ce
Soufiane Dardari <s.dardari@stag.technofuturtic.education>
2024-09-16 14:52:48 +0200

    branch dev

--- Tournoi/src/main/java/be/TFTIC/Tournoi/TournoiApplication.java
+++ Tournoi/src/main/java/be/TFTIC/Tournoi/TournoiApplication.java
@@ -6,8 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TournoiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TournoiApplication.class, args);
-	}
-
+// la branche dev
 }
