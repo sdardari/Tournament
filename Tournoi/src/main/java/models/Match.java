17eeeee462f78b9fa5571b2cd533a6496b26ed18
Xavier Simonet <simonetxavier24@gmail.com>
2024-09-16 15:38:40 +0200

    Entities

---
+++ Tournoi/src/main/java/models/Match.java
@@ -0,0 +1,35 @@
+package models;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "matches")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long matchId;
+
+    @ManyToOne
+    @JoinColumn(name = "team_1_id")
+    private Team team1;
+
+    @ManyToOne
+    @JoinColumn(name = "team_2_id")
+    private Team team2;
+
+    private Integer scoreTeam1;
+    private Integer scoreTeam2;
+
+    @ManyToOne
+    @JoinColumn(name = "winner_team_id")
+    private Team winner;
+
+    // Many-to-one relation with Tournament
+    @ManyToOne
+    @JoinColumn(name = "tournament_id")
+    private Tournament tournament;
+}
---
+++ Tournoi/src/main/java/models/Venue.java
@@ -0,0 +1,27 @@
+package models;
+
+import lombok.*;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "venues")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Venue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long venueId;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(nullable = false)
+    private String lieu;
+
+    // One-to-many relation with Tournaments
+    @OneToMany(mappedBy = "venue")
+    private List<Tournament> tournaments = new ArrayList<>();
+}
