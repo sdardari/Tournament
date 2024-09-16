2bb0c8eca5feda2b3d03bf3db3db4b3acd2a926d
Soufiane Dardari <s.dardari@stag.technofuturtic.education>
2024-09-16 14:46:15 +0200

    FirstEverPush

---
+++ Tournoi/src/main/java/be/TFTIC/Tournoi/TournoiApplication.java
@@ -0,0 +1,13 @@
+package be.TFTIC.Tournoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournoiApplication.class, args);
+	}
+
+}
---
+++ Tournoi/src/test/java/be/TFTIC/Tournoi/TournoiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.TFTIC.Tournoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
